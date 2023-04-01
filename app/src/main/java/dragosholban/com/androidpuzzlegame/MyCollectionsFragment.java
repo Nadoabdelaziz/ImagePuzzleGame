1cc91330fbcb79dbba30ee6bd5c81e7e8bf7478e
Nada Mohamed <nado2000avvwad@gmail.com>
2023-04-01 17:12:07 +0200

    Collection Fragment Done

--- app/src/main/java/dragosholban/com/androidpuzzlegame/MyCollectionsFragment.java
+++ app/src/main/java/dragosholban/com/androidpuzzlegame/MyCollectionsFragment.java
@@ -1,15 +1,26 @@
 package dragosholban.com.androidpuzzlegame;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 public class MyCollectionsFragment extends Fragment {
+    protected View mView;
+    Context context;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.my_collections, viewGroup, false);
+        View view = inflater.inflate(R.layout.my_collections, viewGroup, false);
+        this.mView = view;
+
+        ImageView lineColorCode = (ImageView)view.findViewById(R.id.a);
+        int color = Color.parseColor("#9C27B0"); //The color u want
+        lineColorCode.setColorFilter(color);
+        return view;
     }
 }
